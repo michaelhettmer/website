5b3ec44e0cac332bb099a0bd7fbb6e913f05a3ae
Michael Hettmer <mail@michael-hettmer.de>
2020-03-03 09:55:36 +0100

    feat(home): add basic animation to greeting text

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useTrail, animated } from 'react-spring';
 import SEO from '~/components/SEO';
 import styles from './Home.module.css';
 
+const items = ['Hi.', 'My name is', 'Michael'];
+const config = { mass: 5, tension: 2000, friction: 200 };
+
 export default () => {
+    const [toggle, setToggle] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setToggle(true), 200);
+    }, []);
+
+    const trail = useTrail(items.length, {
+        config,
+        opacity: toggle ? 1 : 0,
+        x: toggle ? 0 : 20,
+        height: toggle ? 80 : 0,
+        from: { opacity: 0, x: 20, height: 0 },
+    });
+
     return (
         <>
             <SEO title="Home" />
@@ -11,11 +29,13 @@ export default () => {
                     <div className={styles.TopContent}>
                         <div className={styles.Avatar}>MH</div>
                         <span className={styles.Greeting}>
-                            Hi.
-                            <br />
-                            My name is
-                            <br />
-                            Michael
+                            {trail.map(({ x, height, ...rest }, index) => (
+                                <animated.div
+                                    key={items[index]}
+                                    style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+                                    <animated.div style={{ height }}>{items[index]}</animated.div>
+                                </animated.div>
+                            ))}
                         </span>
                     </div>
                 </div>
