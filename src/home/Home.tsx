4a8d3ca2fee281c6cd5761cfa13bc6c1aca4a20d
Michael Hettmer <mail@michael-hettmer.de>
2020-03-03 01:15:05 +0100

    fix(home): change color code to be more greenish

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import SEO from '~/components/SEO';
+import styles from './Home.module.css';
 
 export default () => {
     return (
         <>
             <SEO title="Home" />
-            <span>Hello Gatsby</span>
+            <div className={styles.Root}>
+                <div className={styles.Top}>
+                    <div className={styles.TopContent}>
+                        <div className={styles.Avatar}>MH</div>
+                        <span className={styles.Greeting}>
+                            Hi.
+                            <br />
+                            My name is
+                            <br />
+                            Michael
+                        </span>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
